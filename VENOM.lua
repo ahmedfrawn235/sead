19a06665ed1aede46cd0b585d21819be4e31777a
ahmedfrawn235 <94080598+ahmedfrawn235@users.noreply.github.com>
2021-11-24 14:11:37 +0200

    Add files via upload

--- VENOM.lua
+++ VENOM.lua
@@ -3874,7 +3874,7 @@ keyboard.inline_keyboard = {
 {{text = '◗اضافه البوت لمجمعتك◖', url="http://t.me/"..sudos.UserName.."?startgroup=new"}},
 }
 local msg_id = msg.id_/2097152/0.5 
-https.request("https://api.telegram.org/bot"..token..'/sendvideo?chat_id=' .. msg.chat_id_ .. '&video=https://t.me/source_frawn/183&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendvideo?chat_id=' .. msg.chat_id_ .. '&video=https://t.me/source_frawn/185&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 if text ==  'صياد '  then
 local Text = [[
@@ -3886,7 +3886,7 @@ keyboard.inline_keyboard = {
 {{text = ' 𝗗𝙀𝗩 𝙀𝗟 𝗦𝙀𝗔𝗗' ,url="t.me/UU_ML1"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..' /sendPhoto?chat_id=  '.. msg.chat_id_ .. '&photo=https://t.me/UU_ML1&caption=  '.. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..' /sendPhoto?chat_id='.. msg.chat_id_ ..'&photo=https://t.me/UU_ML1&caption= '.. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 if text =='فرعون ' then
 local Text = [[
