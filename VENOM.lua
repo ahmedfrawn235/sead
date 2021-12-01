ff310f7fe40301038c96e7452bf45e1359d48a20
ahmedfrawn235 <94080598+ahmedfrawn235@users.noreply.github.com>
2021-12-01 06:52:15 +0200

    Add files via upload

--- VENOM.lua
+++ VENOM.lua
@@ -6295,7 +6295,6 @@ end,nil)
 end,nil) 
 end 
 end
-
 if text == 'تفعيل الحمايه'and Mod(msg) and msg.reply_to_message_id_ == 0 then 
 database:set(bot_id.."lock:Contact"..msg.chat_id_,'del')  
 database:set(bot_id.."lock:Spam"..msg.chat_id_,'del')  
@@ -6319,6 +6318,7 @@ tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,dat
 send(msg.chat_id_, msg.id_,' ◍ تم تعطيل جلب الصوره ،\n ◍ تم قفل البوتات  ،\n ◍ تم قفل التكرار ،\n ◍ تم قفل الروابط ،\n ◍ تم قفل التوجيه ،\n ◍ تم قفل الملصقات ،\n ◍ تم قفل المتحركه ،\n ◍ تم قفل الفيديو ،\n ◍ تم قفل الفشار ،\n ◍ تم قفل الانلاين ،\n ◍ تم قفل تعديل المديا ،\n ◍ تم وضع التكرار  ،\n ◍ تم قفل الفارسيه  ،\n ◍ تم وضع الايدي بدون صوره ،n  \n ◍ تم تفعيل الحمايه  بواسطه ، [「'..Rutba(msg.sender_user_id_,msg.chat_id_)..'」](T.ME/'..(data.username_ or 'textchuser')..')  ')
 end,nil)   
 end
+
 if text == 'تعطيل الحمايه'and Mod(msg) and msg.reply_to_message_id_ == 0 then 
 database:del(bot_id.."lock:inline"..msg.chat_id_)  
 database:del(bot_id.."lock:Lock:Sexy"..msg.chat_id_)  
@@ -6338,6 +6338,7 @@ tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,dat
 send(msg.chat_id_, msg.id_,' ◍ تم تفعيل جلب الصوره ،\n ◍ تم  فتح البوتات  ،\n ◍ تم  فتح التكرار ،\n ◍ تم  فتح الروابط ،\n ◍ تم  فتح التوجيه ،\n ◍ تم  فتح الملصقات ،\n ◍ تم  فتح المتحركه ،\n ◍ تم  فتح الفيديو ،\n ◍ تم  فتح الفشار ،\n ◍ تم  فتح الانلاين ،\n ◍ تم  فتح تعديل المديا ،\n ◍ تم وضع التكرار  ،\n ◍ تم  فتح الفارسيه  ،\n ◍ تم وضع الايدي بصوره،n \n ◍ تم تفعيل الحمايه  بواسطه ، [「'..Rutba(msg.sender_user_id_,msg.chat_id_)..'」](T.ME/'..(data.username_ or 'textchuser')..')  ')
 end,nil)   
 end
+
 if text == 'تفعيل الحمايه القصوه'and Mod(msg) and msg.reply_to_message_id_ == 0 then 
 database:set(bot_id.."lock:text"..msg.chat_id_,true) 
 database:set(bot_id.."lock:AddMempar"..msg.chat_id_,'kick')
@@ -6367,6 +6368,7 @@ tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,dat
 send(msg.chat_id_, msg.id_,'تم قفل الحمايه كامله\nتحظير هذا الحمايه تقفل كل حاجه بلتقييد\nتفعيل الحمايه دي بس عند التفليش او التحفيل\nلتعطيل الحمايه القصوه\nاكتب تعطيل الحمايه القوصه\n تم تفعيل الحمايه القصوه من قبل [「'..Rutba(msg.sender_user_id_,msg.chat_id_)..'」](T.ME/'..(data.username_ or 'textchuser')..')  ')
 end,nil)   
 end
+
 if text == 'تعطيل الحمايه القصوه'and Mod(msg) and msg.reply_to_message_id_ == 0 then 
 database:del(bot_id.."lock:Spam"..msg.chat_id_)  
 database:del(bot_id.."lock:Join"..msg.chat_id_)  
@@ -6391,12 +6393,12 @@ tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,dat
 send(msg.chat_id_, msg.id_,'  تم تعطيل الحمايه القصوه من قبل    [「'..Rutba(msg.sender_user_id_,msg.chat_id_)..'」](T.ME/'..(data.username_ or 'textchuser')..')  ')
 end,nil)   
 end
+
 if text == 'قفل الدردشه' and msg.reply_to_message_id_ == 0 and Manager(msg) then 
 bot_data:set(ban_id.."lock:text"..msg.chat_id_,true) 
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data)  
 send(msg.chat_id_, msg.id_,'⋆  مـن قبـل  ⤌ [『'..Rutba(msg.sender_user_id_,msg.chat_id_)..'』](T.ME/'..(data.username_ or 'textchuser')..') \n⋆ تـم قفـل الدردشه ')
 end,nil)
-
 elseif text == 'قفل الاضافه' and msg.reply_to_message_id_ == 0 and Mod(msg) then 
 bot_data:set(ban_id.."lock:AddMempar"..msg.chat_id_,'kick')  
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data) 
